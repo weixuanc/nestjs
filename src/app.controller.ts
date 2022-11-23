365a01e8f86ac571be89ac6514cf7347c12d6e60
chenweixuan <wxchen@creekspring.com>
2022-11-23 17:58:07 +0800

    1、添加路由版本号； 2、添加统一成功响应拦截器

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  path: 'user',
+  // version: '1'
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('test')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  @Version('2') // 方法的version会覆盖控制器的version
+  getHello1(): string {
+    return 'test2';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 接口版本化管理
+  app.enableVersioning({
+    // defaultVersion: '1', // 默认版本号,全局配置，优先级最低
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'], // 配置多版本
+    type: VersioningType.URI
+  })
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
